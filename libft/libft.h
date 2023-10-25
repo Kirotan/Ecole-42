76dc4bd530d3d747fd8d915bb9bbe0de0dbf114a
Guillaume Doumer <gdoumer@k2r3p8.42mulhouse.fr>
2023-10-25 14:25:45 +0200

    Ajout strdup

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gdoumer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 11:29:13 by gdoumer           #+#    #+#             */
-/*   Updated: 2023/10/25 13:04:12 by gdoumer          ###   ########.fr       */
+/*   Updated: 2023/10/25 14:22:38 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_atoi(const char *nptr);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strdup(const char *s);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
