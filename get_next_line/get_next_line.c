0a2756f51c3181af9db16744b84075825835d426
Guillaume Doumer <gdoumer@k0r1p12.42mulhouse.fr>
2023-11-13 16:09:07 +0100

    debut gnl

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdoumer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:37:55 by gdoumer           #+#    #+#             */
+/*   Updated: 2023/11/13 16:08:17 by gdoumer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+// Utiliser read pour ajouter les caracteres a la reserve.
+void	read_and_stach()
+{
+
+}
+
+// Ajouter le contenu du buffer a la fin de la reserve
+void	add_to_stach()
+{
+
+}
+
+// Extrait tous les caracteres depuis la reserve et les stockent dans out_line. Arretez apres le 1er \n rencontre.
+void	extract_line()
+{
+
+}
+
+// Nettoie la reserve de sorte que seuls les caracteres qui n'ont pas ete retrounes a la fin de get_next_line reste dans la static reserve.
+void	clean_stach
+{
+
+}
+
+char	*get_next_line(int fd)
+{
+
+}
