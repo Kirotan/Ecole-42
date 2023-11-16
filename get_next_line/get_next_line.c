2186b21a3f16f72a68c408639ab650517514ccf1
Guillaume Doumer <gdoumer@k0r1p13.42mulhouse.fr>
2023-11-16 16:03:17 +0100

    essai2

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,38 +6,70 @@
 /*   By: gdoumer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 15:37:55 by gdoumer           #+#    #+#             */
-/*   Updated: 2023/11/15 18:42:27 by gdoumer          ###   ########.fr       */
+/*   Updated: 2023/11/16 16:02:51 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// Utiliser read pour ajouter les caracteres a la reserve.
-char	*read_and_stach(int fd, size_t nbyte)
+// Utiliser read pour ajouter les caracteres au buffer.
+char	*read_and_buf(int fd)
 {
 	char		*buf;
-	static char	**stach;
-	char		*line;
+	static char	**stash;
 	int			readed;
 
 	if(buffer_size <= 0 || fd < 0 || fd > 1024)
 		return (NULL);
-	buf = NULL;
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	ft_bzero(buf, BUFFER_SIZE);
 	readed = 1;
 	while(readed > 0)
 	{
 		readed = read(fd, buf, BUFFER_SIZE);
-		stash[fd] = strjoin(stash[d], buff);
+		if (readed == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		if (!(stash == add_buf_to_stash(fd, buf, stash)))
+			break;
 	}
+	return (*stash);
 }
 
 // Ajouter le contenu du buffer a la fin de la reserve
-void	add_to_stach()
+char	*add_buf_to_stash(int fd, char *buf, char **stash)
 {
+	char	*tmp;
+	char	*new_stash;
 
+	if (!stash[fd])
+	{
+		stash[fd] = strdup(buff);
+		if (!stash[fd])
+			return (NULL);
+	}	
+	else
+	{
+		tmp = strdup(stash[fd]);
+		if (!tmp)
+			return (NULL);
+		new_stash = strjoin(tmp, buff);
+		free(tmp);
+		if (!new_stach)
+			return (NULL);
+		free(stash[fd]);
+		stash[fd] = new_stash;
+	}
+	if (strchr(buf, '\n'))
+		return (NULL);
+	return (stash[fd]);
 }
 
-// Extrait tous les caracteres depuis la reserve et les stockent dans out_line. Arretez apres le 1er \n rencontre.
+// Extrait tous les caracteres depuis la reserve et les stockent dans new_line. Arretez apres le 1er \n rencontre.
 void	extract_line()
 {
 
