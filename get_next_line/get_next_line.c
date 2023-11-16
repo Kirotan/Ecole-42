d48dfaeda32b8923b8afc65ab0012542367fc64f
Guillaume Doumer <gdoumer@k0r1p13.42mulhouse.fr>
2023-11-16 17:09:51 +0100

    essai3

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,17 +6,14 @@
 /*   By: gdoumer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 15:37:55 by gdoumer           #+#    #+#             */
-/*   Updated: 2023/11/16 16:02:51 by gdoumer          ###   ########.fr       */
+/*   Updated: 2023/11/16 17:09:33 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// Utiliser read pour ajouter les caracteres au buffer.
-char	*read_and_buf(int fd)
+char	*read_and_buf(int fd, char *buf, char **stash)
 {
-	char		*buf;
-	static char	**stash;
 	int			readed;
 
 	if(buffer_size <= 0 || fd < 0 || fd > 1024)
@@ -40,7 +37,6 @@ char	*read_and_buf(int fd)
 	return (*stash);
 }
 
-// Ajouter le contenu du buffer a la fin de la reserve
 char	*add_buf_to_stash(int fd, char *buf, char **stash)
 {
 	char	*tmp;
@@ -48,7 +44,7 @@ char	*add_buf_to_stash(int fd, char *buf, char **stash)
 
 	if (!stash[fd])
 	{
-		stash[fd] = strdup(buff);
+		stash[fd] = strdup(buf);
 		if (!stash[fd])
 			return (NULL);
 	}	
@@ -57,9 +53,9 @@ char	*add_buf_to_stash(int fd, char *buf, char **stash)
 		tmp = strdup(stash[fd]);
 		if (!tmp)
 			return (NULL);
-		new_stash = strjoin(tmp, buff);
+		new_stash = strjoin(tmp, buf);
 		free(tmp);
-		if (!new_stach)
+		if (!new_stash)
 			return (NULL);
 		free(stash[fd]);
 		stash[fd] = new_stash;
@@ -69,16 +65,28 @@ char	*add_buf_to_stash(int fd, char *buf, char **stash)
 	return (stash[fd]);
 }
 
-// Extrait tous les caracteres depuis la reserve et les stockent dans new_line. Arretez apres le 1er \n rencontre.
-void	extract_line()
+char	*clean_stach(int fd, char **stash)
 {
+	int		end;
+	char	*line;
+	char	*time;
 
-}
-
-// Nettoie la reserve de sorte que seuls les caracteres qui n'ont pas ete retrounes a la fin de get_next_line reste dans la static reserve.
-void	clean_stach
-{
-
+	end = 0;
+	while (stash[fd][end] && stash[fd][end] != '\n')
+		end++;
+	if (stash[fd][end] && stash[fd][end] == '\n')
+		end++;
+	line = ft_substr(stash[fd], 0, end);
+	if (!line)
+		return (NULL);
+	time = ft_substr(stash[fd], end, ft_strlen((stash[fd]) - end));
+	if (!time);
+		return (NULL);
+	free(stash[fd]);
+	stash[fd] = time;
+	if (!stash[fd])
+		return (NULL);
+	return (line);
 }
 
 char	*get_next_line(int fd)
