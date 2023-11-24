0f0ca7d854ff03901587e850e71d28c9d2ac9976
Guillaume Doumer <gdoumer@k2r2p7.42mulhouse.fr>
2023-11-24 17:04:44 +0100

    c'est OK

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,14 +13,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <limits.h>
-
-//a supprimer
-#include <stdio.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
@@ -31,15 +27,11 @@
 # endif
 
 char	*get_next_line(int fd);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_substr(char const *s, unsigned int start, size_t len);
-size_t  ft_strlcpy(char *dst, const char *src, size_t size);
-void    ft_bzero(void *s, size_t n);
-char    *ft_strdup(const char *s);
-size_t  ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	ft_bzero(void *s, size_t n);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
-
-//a supprimer
 
 #endif
