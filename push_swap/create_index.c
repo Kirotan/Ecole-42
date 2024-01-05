9bf60435099ef1f8aa75b895256705a704c79e05
Guillaume Doumer <gdoumer@k2r2p1.42mulhouse.fr>
2024-01-05 23:50:03 +0100

    28

--- push_swap/create_index.c
+++ push_swap/create_index.c
@@ -6,19 +6,23 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 14:38:53 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/05 23:33:06 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/05 23:49:46 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	fill_index(t_dlist **dl_a, long mini_value, size_t *index)
+static void	fill_index(t_dlist **dl_a, long mini_value, size_t *index, size_t length)
 {
-	while (*index != (*dl_a)->index)
+	size_t	k;
+
+	k = 0;
+	while (k < length)
 	{
 		if (mini_value == (*dl_a)->data)
 			(*dl_a)->index = *index;
 		(*dl_a) = (*dl_a)->dl_next;
+		k++;
 	}
 }
 
@@ -33,7 +37,16 @@ static long	find_mini_value(t_dlist **dl_a, size_t length)
 	mini_value = tmp->data;
 	while (j < length)
 	{
-		if (tmp->data < mini_value)
+		if (!tmp->index)
+			mini_value = tmp->data;
+		tmp = tmp->dl_next;
+		j++;
+	}
+	j = 0;
+	tmp = (*dl_a);
+	while (j < length)
+	{
+		if (tmp->data < mini_value && !tmp->index)
 			mini_value = tmp->data;
 		tmp = tmp->dl_next;
 		j++;
@@ -52,7 +65,7 @@ void	create_index(t_dlist **dl_a, size_t length)
 	while (i < length)
 	{
 		minimal_value = find_mini_value(dl_a, length);
-		fill_index(dl_a, minimal_value, &index);
+		fill_index(dl_a, minimal_value, &index, length);
 		index ++;
 		i++;
 	}
