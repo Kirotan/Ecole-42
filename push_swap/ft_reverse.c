d7b87c74dfffb6587a662735318e4a61365324c3
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-06 18:44:43 +0100

    32.2

--- push_swap/ft_reverse.c
+++ push_swap/ft_reverse.c
@@ -6,8 +6,37 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 14:43:00 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/06 14:47:40 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/06 18:43:57 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	change_last(t_dlist **double_list)
+{
+	*double_list = (*double_list)->dl_prev;
+}
+
+void	reverse_both(t_dlist **dl_a, t_dlist **dl_b)
+{
+	if (*dl_a && *dl_b)
+	{
+		change_last(dl_a);
+		change_last(dl_b);
+	}
+	ft_printf("rrr\n");
+}
+
+void	reverse_b(t_dlist **dl_b)
+{
+	if (*dl_b)
+		change_last(dl_b);
+	ft_printf("rrb\n");
+}
+
+void	reverse_a(t_dlist **dl_a)
+{
+	if (*dl_a)
+		change_last(dl_a);
+	ft_printf("rra\n");
+}
