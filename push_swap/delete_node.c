d5d83f61b37f91cd20b1f43dd1fd9dc6b0503bb5
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-06 19:13:34 +0100

    32.4

--- push_swap/delete_node.c
+++ push_swap/delete_node.c
@@ -6,15 +6,15 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 18:57:06 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/06 19:07:32 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/06 19:13:21 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	delete_node(t_dlist **double_list, t_dlist	*node_to_delete)
+void	delete_node(t_dlist **double_list, t_dlist *node_to_delete)
 {
-	if (!double_list || !node_to_delete || length_double_list(double_list) = 1)
+	if (!double_list || !node_to_delete || length_double_list(double_list) == 1)
 		return ;
 	if (*double_list == node_to_delete)
 	{
