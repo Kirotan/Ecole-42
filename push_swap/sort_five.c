525ca29bb77b48c8b93a47e14b72c0ad7173da6d
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-08 17:18:50 +0100

    33.08

--- push_swap/sort_five.c
+++ push_swap/sort_five.c
@@ -6,37 +6,42 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 15:14:38 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/08 16:55:45 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:18:30 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_five(t_dlist **dl_a, t_dlist **dl_b, int nb_param)
+void	sort_five_median(int nb_param, size_t median, t_dlist **a, t_dlist **b)
 {
-	size_t	median;
 	int		i;
 	int		j;
 	size_t	k;
 
-	median = nb_param / 2;
 	i = 0;
 	j = 0;
-	while (i < nb_param)
+	while (i++ < nb_param)
 	{
-		if ((*dl_a)->index <= median)
+		if ((*a)->index <= median)
 		{
 			k = (size_t)j;
-			push_b(dl_a, dl_b);
-			j++;
-			if ((size_t)j == median)
+			push_b(a, b);
+			if ((size_t)j++ == median)
 				break ;
 			else if (k != (size_t)j)
 				continue ;
 		}
-		rotate_a(dl_a);
-		i++;
+		rotate_a(a);
 	}
+
+}
+
+void	sort_five(t_dlist **dl_a, t_dlist **dl_b, int nb_param)
+{
+	size_t	median;
+
+	median = nb_param / 2;
+	sort_five_median(nb_param, median, dl_a, dl_b);
 	sort_three_a(dl_a);
 	if ((*dl_b)->index < (*dl_b)->dl_next->index)
 		swap_b(dl_b);
