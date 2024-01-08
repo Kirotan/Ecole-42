0a9e9c2a0da1f010914c993fc824a1232c9bf8f9
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-08 17:47:35 +0100

    33.10

--- push_swap/algorithm.c
+++ push_swap/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:47:09 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/08 17:39:52 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:45:39 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	algorithm(int nb_param, t_dlist **dl_a, t_dlist **dl_b)
 		sort_two_a(dl_a);
 	if (nb_param == 3)
 		sort_three_a(dl_a);
-	if (nb_param == 5)
+	if (nb_param >= 4 && nb_param <= 5)
 		sort_five(dl_a, dl_b, nb_param);
 }
