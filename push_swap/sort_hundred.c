0214e5c343ba0dbb7458a232727742a660775de5
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-09 16:39:07 +0100

    33.12

--- push_swap/sort_hundred.c
+++ push_swap/sort_hundred.c
@@ -6,32 +6,51 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:52:21 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/08 18:41:44 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/09 16:38:51 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
+
+size_t	position_averag(t_dlist **dlist, size_t len_list, size_t index_position)
+{
+	size_t	x_one;
+	size_t	y_two;
+	size_t	z_three;
+	size_t	average;
+	size_t	i;
+
+	i = 1;
+	x_one = 0;
+	y_two = 0;
+	z_three = 0;
+	while (i < len_list)
+	{
+		if ((*dlist)->index == index_position)
+			x_one = i;
+		if ((*dlist)->index == index_position + 1)
+			y_two = i;
+		if ((*dlist)->index == index_position + 2)
+			z_three = i;
+		(*dlist)->dl_next;
+		i++;
+	}
+	average = (x_one + y_two + z_three) / 3;
+	return (average);
+}
+
 void	sort_hundred(t_dlist **dl_a, t_dlist **dl_b, int nb_param)
 {
-	int	i;
+	size_t	index_position;
+	size_t	len_a;
+	size_t	len_b;
+	size_t	average;
 
-	i = 0;
-	while (i++ < nb_param - 3)
-	{
-		if (i == 3)
-			sort_three_b(dl_b);
-		if ((*dl_a)->index > (*dl_a)->dl_next->index)
-		{
-			swap_a(dl_a);
-			push_b(dl_a, dl_b);
-		}
-		else if ((*dl_a)->index > (*dl_a)->dl_prev->index)
-		{
-			rotate_a(dl_a);
-			push_b(dl_a, dl_b);
-		}
-		else
-			push_b(dl_a, dl_b);
-	}
+	index_position = 1;
+	len_a = 0;
+	len_b = 0;
+	average = 0;
+
 }
