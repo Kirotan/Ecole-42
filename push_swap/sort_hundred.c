bf99d88b5b49c0c6d27f07874679c6ea887c5fb0
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-10 18:01:23 +0100

    35

--- push_swap/sort_hundred.c
+++ push_swap/sort_hundred.c
@@ -6,14 +6,24 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:52:21 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/10 15:04:44 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/10 18:00:54 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	rest_of_b(t_dlist **dl_a)
+int	rest_of_b(t_dlist **dl_a, t_dlist **dl_b)
 {
+	if (length_double_list(dl_b) == 2)
+	{
+		sort_two_b(dl_b);
+		return (0);
+	}
+	if (length_double_list(dl_b) == 3)
+	{
+		sort_three_b(dl_b);
+		return (0);
+	}
 	if (length_double_list(dl_a) == 2)
 	{
 		sort_two_a(dl_a);
@@ -30,71 +40,72 @@ int	rest_of_b(t_dlist **dl_a)
 void	rotate_list(t_dlist **dl_a, t_dlist **dl_b, size_t index_pos)
 {
 	size_t	i;
+	size_t	j;
 	int		sort_or_not;
 
 	i = 0;
+	j = 0;
 	while (i < length_double_list(dl_a))
 	{
 		if ((*dl_a)->index == index_pos || (*dl_a)->index == index_pos + 1
 			|| (*dl_a)->index == index_pos + 2)
 		{
 			push_b(dl_a, dl_b);
-			if (length_double_list(dl_b) > 0
-				&& (*dl_b)->index < (*dl_b)->dl_next->index)
+			if ((*dl_b)->index < (*dl_b)->dl_next->index)
 				swap_b(dl_b);
-			sort_or_not = rest_of_b(dl_a);
+			sort_or_not = rest_of_b(dl_a, dl_b);
 			if (sort_or_not == 1)
 				return ;
-			if (i > 3)
+			i++;
+			j++;
+			if (j > 2)
 				break ;
+			if (j < 4)
+				continue ;
 		}
-		rotate_a(dl_a);
 		i++;
+		rotate_a(dl_a);
 	}
 }
 
 void	reverse_list(t_dlist **dl_a, t_dlist **dl_b, size_t index_pos)
 {
 	size_t	i;
+	size_t	j;
 	int		sort_or_not;
 
 	i = 0;
-	while (length_double_list(dl_a) > i)
+	j = 0;
+	while (i < length_double_list(dl_a))
 	{
 		if ((*dl_a)->index == index_pos || (*dl_a)->index == index_pos + 1
 			|| (*dl_a)->index == index_pos + 2)
 		{
 			push_b(dl_a, dl_b);
-			if (length_double_list(dl_b) > 1
-				&& (*dl_b)->index < (*dl_b)->dl_next->index)
-				swap_b(dl_b);
-			sort_or_not = rest_of_b(dl_a);
+			sort_or_not = rest_of_b(dl_a, dl_b);
 			if (sort_or_not == 1)
 				return ;
-			if (i > 3)
+			i++;
+			j++;
+			if (j > 3)
 				break ;
+			if (j < 3)
+				continue ;
 		}
-		reverse_a(dl_a);
 		i++;
+		reverse_a(dl_a);
 	}
 }
 
 void	fill_b(t_dlist **dl_a, t_dlist **dl_b, size_t aver_a, size_t index_pos)
 {
-	int	i;
-
-	i = 0;
-	while (i < 3)
+	if (aver_a >= length_double_list(dl_a) / 2)
 	{
-		if (aver_a >= length_double_list(dl_a) / 2)
-		{
-			reverse_list(dl_a, dl_b, index_pos);
-		}
-		else if (aver_a < length_double_list(dl_a) / 2)
-		{
-			rotate_list(dl_a, dl_b, index_pos);
-		}
-		i++;
+		reverse_list(dl_a, dl_b, index_pos);
+	}
+	else if (aver_a < length_double_list(dl_a) / 2)
+	{
+		rotate_list(dl_a, dl_b, index_pos);
 	}
 }
 
@@ -106,7 +117,7 @@ void	sort_hundred(t_dlist **dl_a, t_dlist **dl_b)
 	size_t	i;
 
 	i = 0;
-	index_pos = 0;
+	index_pos = 1;
 	tmp = length_double_list(dl_a);
 	while (index_pos < tmp)
 	{
