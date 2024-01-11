688f8f8bc079cba421c13d1ef4d23bc9d52efc8c
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-01-11 13:06:14 +0100

    35.6

--- push_swap/sort_hundred.c
+++ push_swap/sort_hundred.c
@@ -6,11 +6,28 @@
 /*   By: gdoumer <gdoumer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:52:21 by gdoumer           #+#    #+#             */
-/*   Updated: 2024/01/10 20:04:29 by gdoumer          ###   ########.fr       */
+/*   Updated: 2024/01/11 12:59:07 by gdoumer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	handle_case(t_dlist	**dl_a, t_dlist **dl_b)
+{
+	if ((*dl_b) && (*dl_b)->dl_next != *dl_b
+		&& (*dl_a)->index < (*dl_b)->index
+		&& (*dl_a)->index < (*dl_b)->dl_next->index)
+	{
+		rotate_b(dl_b);
+		push_b(dl_a, dl_b);
+		swap_b(dl_b);
+		reverse_b(dl_b);
+	}
+	else
+		push_b(dl_a, dl_b);
+	if ((*dl_b)->index < (*dl_b)->dl_next->index)
+		swap_b(dl_b);
+}
 
 void	change_list(t_dlist **dl_a, t_dlist **dl_b, size_t index_pos)
 {
@@ -24,19 +41,7 @@ void	change_list(t_dlist **dl_a, t_dlist **dl_b, size_t index_pos)
 		if ((*dl_a)->index == index_pos || (*dl_a)->index == index_pos + 1
 			|| (*dl_a)->index == index_pos + 2)
 		{
-			if ((*dl_b) && (*dl_b)->dl_next != *dl_b
-				&& (*dl_a)->index < (*dl_b)->index
-				&& (*dl_a)->index < (*dl_b)->dl_next->index)
-			{
-				rotate_b(dl_b);
-				push_b(dl_a, dl_b);
-				swap_b(dl_b);
-				reverse_b(dl_b);
-			}
-			else
-				push_b(dl_a, dl_b);
-			if ((*dl_b)->index < (*dl_b)->dl_next->index)
-				swap_b(dl_b);
+			handle_case(dl_a, dl_b);
 			i++;
 			j++;
 		}
