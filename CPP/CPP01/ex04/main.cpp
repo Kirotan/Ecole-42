4cd0ef2a3b94c896f37c72104924bd9edf85e68e
Guillaume Doumer <gdoumer@k1r3p12.42mulhouse.fr>
2024-08-22 17:45:00 +0200

    cpp 01 exo 4

--- CPP/CPP01/ex04/main.cpp
+++ CPP/CPP01/ex04/main.cpp
@@ -5,6 +5,8 @@
 
 typedef std::string str;
 
+str	replaceAll(str &str, const std::string &from, const std::string &to);
+
 int	main(int argc, char **argv) {
 
 	std::ifstream		*file;
@@ -23,6 +25,10 @@ int	main(int argc, char **argv) {
 
 	file = new std::ifstream;
 	file->open(argv[1]);
+	if (file->good() == false) {
+		std::cerr << "File error." << std::endl;
+		exit(1);
+	}
 	buff << file->rdbuf();
 	text = buff.str();
 	file->close();
@@ -30,9 +36,33 @@ int	main(int argc, char **argv) {
 
 	new_file = new std::ofstream;
 	new_file->open(new_name.c_str());
+
+
+	if (text.find(argv[2]) == std::string::npos) {
+		std::cout << "Occurence not found." << std::endl;
+		exit(1);
+	}
+
+	text = replaceAll(text, argv[2], argv[3]);
 	new_file->write(text.c_str(), text.size());
 	new_file->close();
 	delete new_file;
 
 	return 0;
+}
+
+str	replaceAll(str &text, const std::string &from, const std::string &to) {
+
+	str			result;
+	size_t		startPos = 0;
+	size_t		foundPos;
+
+	while((foundPos = text.find(from, startPos)) != str::npos) {
+		result.append(text, startPos, foundPos - startPos);
+		result.append(to);
+		startPos = foundPos + from.length();
+	}
+	result.append(text, startPos, text.length() - startPos);
+	text = result;
+	return text;
 }
