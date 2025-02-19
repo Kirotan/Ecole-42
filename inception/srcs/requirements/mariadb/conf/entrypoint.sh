135e3d0da3f1718a88f60663fe2a6a5f2b1df39b
Guillaume Doumer <gdoumer@k1r2p13.42mulhouse.fr>
2025-02-19 19:03:43 +0100

    inception 1.3

--- inception/srcs/requirements/mariadb/conf/entrypoint.sh
+++ inception/srcs/requirements/mariadb/conf/entrypoint.sh
@@ -2,18 +2,18 @@
 set -e
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
 mysqld --skip-networking --socket=/var/run/mysqld/mysqld.sock &
 pid="$!"
 
 until mariadb --user=root --execute="SELECT 1;" &>/dev/null; do
-    sleep 2
+	sleep 2
 done
 
 if [ -f "/docker-entrypoint-initdb.d/init.sql" ]; then
-    mariadb -u root < /docker-entrypoint-initdb.d/init.sql
+	mariadb -u root < /docker-entrypoint-initdb.d/init.sql
 fi
 
 mysqladmin --user=root shutdown
